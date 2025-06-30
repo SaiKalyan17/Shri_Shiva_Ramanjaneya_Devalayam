843cb21ec8c276dc6fd9161386f7a7c237f62dfa
SaiKalyan17 <148944451+SaiKalyan17@users.noreply.github.com>
2025-07-01 00:38:44 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,22 @@
-import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import logo from '../../assets/logo2.png';
 import './Navbar.css';
 
 const Navbar = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
+    
 
     return (
         <nav className="nav-container">
             <img src={logo} alt="Temple Logo" className="logo-image" />
-            <h2> <Link to="/">Shri Shiva Ramanjaneya Devalayam</Link></h2>
+            <h2> <Link to="/admin/login">Shri Shiva Ramanjaneya Devalayam</Link></h2>
             
-            <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+            <div className="menu">
                 <span></span>
                 <span></span>
                 <span></span>
             </div>
 
-            <ul className={`items-list ${menuOpen ? 'show' : ''}`}>
+            <ul className='items-list'>
                 <li><NavLink to="/">Home</NavLink></li>
                 <li><NavLink to="/events">Events</NavLink></li>
                 <li><NavLink to="/gallery">Gallery</NavLink></li>
@@ -29,3 +28,36 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
+// import { useState } from 'react';
+// import { Link, NavLink } from 'react-router-dom';
+// import logo from '../../assets/logo2.png';
+// import './Navbar.css';
+
+// const Navbar = () => {
+//     const [menuOpen, setMenuOpen] = useState(false);
+
+//     return (
+//         <nav className="nav-container">
+//             <img src={logo} alt="Temple Logo" className="logo-image" />
+//             <h2> <Link to="/">Shri Shiva Ramanjaneya Devalayam</Link></h2>
+            
+//             <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+//                 <span></span>
+//                 <span></span>
+//                 <span></span>
+//             </div>
+
+//             <ul className={`items-list ${menuOpen ? 'show' : ''}`}>
+//                 <li><NavLink to="/">Home</NavLink></li>
+//                 <li><NavLink to="/events">Events</NavLink></li>
+//                 <li><NavLink to="/gallery">Gallery</NavLink></li>
+//                 <li><NavLink to="/donations">Donations</NavLink></li>
+//                 <li><NavLink to="/about">About</NavLink></li>
+//             </ul>
+//         </nav>
+//     );
+// };
+
+// export default Navbar;
